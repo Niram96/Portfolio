13b5944ec175a1395b5987508132dc7efc5dbdf8
Marin <marin.desloges@sfr.fr>
2023-11-09 17:54:06 +0100

    Ajout lien LinkedIn

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,12 +1,19 @@
 import '../../utils/styles/footer.scss';
-import gitHubLogo from '../../assets/icons/github-mark/github-mark-white.svg';
+import gitHubLogo from '../../assets/icons/github.svg';
+import linkedInLogo from '../../assets/icons/linkedin.png';
+
 
 function Footer () {
     return (
         <footer>
-            <a href='https://github.com/Niram96'>
-                <img src={gitHubLogo} alt='Logo GitHub'/>
-            </a>
+            <div className='footer-links'>
+                <a href='https://github.com/Niram96'>
+                    <img src={gitHubLogo} alt='Logo GitHub'/>
+                </a>
+                <a href='https://linkedin.com/in/marin-desloges-2b0a2029a'>
+                    <img src={linkedInLogo} alt='Logo LinkedIn'/>
+                </a>
+            </div>
             <a className="link-top" href='#top'>Haut de page</a>
             <p>Réalisé par Marin Desloges</p>
         </footer>
